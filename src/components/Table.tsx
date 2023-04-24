60a0d55f79a02ca3760ac0283b60612e86be46bf
Diyor Sattarov <sattarovdiyor@gmail.com>
2023-04-24 15:50:16 +0500

    feat: set up the app

---
+++ src/components/Table.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface TableProps {
+  name: string;
+}
+
+const Table: React.FC<TableProps> = () => {
+  return <div></div>;
+};
+
+export default Table;
---
+++ src/pages/tablePage.tsx
@@ -0,0 +1,5 @@
+const tablePage = () => {
+  return <div></div>;
+};
+
+export default tablePage;
